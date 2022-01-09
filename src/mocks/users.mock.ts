675ec1e93bf0128bdbf0fe5bf764d8be16809cb8
Yury Barysevich <matthias@google.com>
2022-01-09 13:45:51 +0300

    feat: create users mock data

---
+++ src/mocks/users.mock.ts
@@ -0,0 +1,44 @@
+export const usersMock = [
+  {
+    id: '98bdc950-6a2d-11ec-a882-d3b7e3716a22',
+    login: 'yury',
+    age: 18,
+    password: 'qwerty',
+    isDeleted: false,
+  },
+  {
+    id: '98bdc950-6a2d-11ec-a882-d3b7e3716a21',
+    login: 'yury5',
+    age: 18,
+    password: 'qwerty',
+    isDeleted: false,
+  },
+  {
+    id: '98bdc950-6a2d-11ec-a882-d3b7e3716a23',
+    login: 'yury1',
+    age: 13,
+    password: 'qwerty',
+    isDeleted: false,
+  },
+  {
+    id: '98bdc950-6a2d-11ec-a882-d3b7e3716a24',
+    login: 'yury2',
+    age: 15,
+    password: 'qwerty',
+    isDeleted: false,
+  },
+  {
+    id: '98bdc950-6a2d-11ec-a882-d3b7e3716a25',
+    login: 'yury3',
+    age: 11,
+    password: 'qwerty',
+    isDeleted: false,
+  },
+  {
+    id: '98bdc950-6a2d-11ec-a882-d3b7e3716a26',
+    login: 'yury4',
+    age: 10,
+    password: 'qwerty',
+    isDeleted: true,
+  },
+];
