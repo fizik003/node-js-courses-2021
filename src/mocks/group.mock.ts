333ef595341b7ada6691a2a6ab8a53934f45d52e
Yury Barysevich <matthias@google.com>
2022-01-09 19:34:58 +0300

    feat:add mock groups data

---
+++ src/mocks/group.mock.ts
@@ -0,0 +1,27 @@
+export const groupsMock = [
+  {
+    id: 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d0',
+    name: 'new',
+    permission: ['READ', 'WRITE'],
+  },
+  {
+    id: 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d1',
+    name: 'new1',
+    permission: ['READ', 'WRITE'],
+  },
+  {
+    id: 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d2',
+    name: 'new2',
+    permission: ['READ', 'WRITE'],
+  },
+  {
+    id: 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d3',
+    name: 'new3',
+    permission: ['READ', 'WRITE'],
+  },
+  {
+    id: 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d4',
+    name: 'new4',
+    permission: ['READ', 'WRITE'],
+  },
+];
