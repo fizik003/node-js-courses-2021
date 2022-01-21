87c7fe2aa98d4c1f3e828f8ac986d17dd9e6f3ef
Yury Barysevich <matthias@google.com>
2022-01-22 00:59:52 +0300

    feat: add group test

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,17 +1,26 @@
 import { userService } from './';
 import { sign, verify } from 'jsonwebtoken';
 import config from '../common/config';
-import { ForbiddenError, NotFoundError, UnauthorizedError } from '../common/errors';
+import {
+  ForbiddenError,
+  NotFoundError,
+  UnauthorizedError,
+} from '../common/errors';
 
 const { JWT_REFRESH_SECRET_KEY, JWT_ACCESS_SECRET_KEY } = config;
 
 export const login = async (login: string, passward: string) => {
   const user = await userService.getByLogin(login);
-  if (!user || user.password !== passward) throw new NotFoundError('invalid login or password');
+  console.log(user);
+
+  if (!user || user.password !== passward)
+    throw new NotFoundError('invalid login or password');
 
   const payload = { id: user.id, login: user.login };
   const accessToken = sign(payload, JWT_ACCESS_SECRET_KEY, { expiresIn: '1h' });
-  const refreshToken = sign(payload, JWT_REFRESH_SECRET_KEY, { expiresIn: '60m' });
+  const refreshToken = sign(payload, JWT_REFRESH_SECRET_KEY, {
+    expiresIn: '60m',
+  });
   return { accessToken, refreshToken };
 };
 
@@ -22,7 +31,9 @@ export const refreshToken = async (token: string) => {
     }
 
     const payload = { id: decoded?.id, login: decoded?.login };
-    const accessToken = sign(payload, JWT_ACCESS_SECRET_KEY, { expiresIn: '10m' });
+    const accessToken = sign(payload, JWT_ACCESS_SECRET_KEY, {
+      expiresIn: '10m',
+    });
     return { accessToken, refreshToken: token };
   });
 };
--- src/tests/router.test.ts
+++ src/tests/router.test.ts
@@ -1,15 +1,18 @@
 import app from '../app';
 import request from 'supertest';
-import { usersMock } from '../mocks';
-import * as userDb from '../data-access/user.db';
+import { groupsMock, usersMock } from '../mocks';
+import { groupDb, userDb } from '../data-access';
 import { v1 as uuidv1 } from 'uuid';
-import { IUser, IUserReq, User } from '../models';
+import { Group, IGroup, IGroupReq, IUser, IUserReq, User } from '../models';
 
 jest.mock('../data-access/user.db');
+jest.mock('../data-access/group.db');
 
 const userDbMock = userDb as jest.Mocked<typeof userDb>;
+const groups = groupsMock as Group[];
 
 const noDeletedUsers = usersMock.filter((user) => !user.isDeleted) as User[];
+const groupDbMock = groupDb as jest.Mocked<typeof groupDb>;
 
 userDbMock.getAll.mockResolvedValue(noDeletedUsers);
 
@@ -38,8 +41,6 @@ userDbMock.getByParams.mockImplementation((subStr: string, limit?: number) => {
 
 userDbMock.remove.mockImplementation((id: string) => {
   const user = noDeletedUsers.find((user) => user.id === id);
-  console.log(id);
-  console.log(user);
 
   if (user) {
     user.isDeleted = true;
@@ -66,6 +67,52 @@ userDbMock.update.mockImplementation(
     }
   }
 );
+
+groupDbMock.get.mockResolvedValue(groups);
+
+groupDbMock.getById.mockImplementation((id: string) =>
+  Promise.resolve(groups.find((group) => group.id === id))
+);
+
+groupDbMock.create.mockImplementation((groupData: IGroupReq) => {
+  const newGroup: IGroup = {
+    id: uuidv1(),
+    ...groupData,
+  };
+
+  groups.push(newGroup as Group);
+  return Promise.resolve(newGroup as Group);
+});
+
+groupDbMock.update.mockImplementation(
+  (id: string, groupData: Partial<IGroupReq>) => {
+    let groupIndex: number;
+    const updatedRows = Object.keys(groupData).length;
+    const group = groups.find((group, index) => {
+      groupIndex = index;
+      return group.id === id;
+    });
+    if (group) {
+      groups[groupIndex] = {
+        ...group,
+        ...groupData,
+      } as Group;
+
+      return Promise.resolve([updatedRows, [groups[groupIndex]]]);
+    }
+  }
+);
+
+groupDbMock.drop.mockImplementation((id: string) => {
+  const groupIndex = groups.findIndex((group) => group.id === id);
+  let returnRes = 0;
+  if (groupIndex + 1) {
+    groups.splice(groupIndex, 1);
+    returnRes = 1;
+  }
+
+  return Promise.resolve(returnRes);
+});
 
 describe('User router', () => {
   let token: string;
@@ -145,5 +192,58 @@ describe('User router', () => {
       .set('Authorization', token);
     expect(res.status).toBe(200);
     expect(res.text).toEqual('"User deleted"');
+  });
+});
+
+describe('Group router', () => {
+  let token: string;
+
+  beforeAll(async () => {
+    const {
+      body: { accessToken },
+    } = await request(app)
+      .post('/login')
+      .send({ login: 'yury', password: 'qwerty' });
+    token = accessToken;
+  });
+
+  it('GET: /group - should return all group', async () => {
+    const countGroups = groupsMock.length;
+    const res = await request(app).get('/group').set('Authorization', token);
+
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(countGroups);
+  });
+
+  it('GET: /group/:id - should return group by id', async () => {
+    const id = 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d1';
+    const res = await request(app)
+      .get(`/group/${id}`)
+      .set('Authorization', token);
+    expect(res.status).toBe(200);
+    expect(res.body.id).toEqual(id);
+  });
+
+  it('POST: /group - should create group', async () => {
+    const countGroups = groups.length;
+    const res = await request(app)
+      .post('/group')
+      .send({ name: 'group', permission: 'READ' })
+      .set('Authorization', token);
+
+    expect(res.status).toBe(200);
+    expect(res.body.name).toEqual('group');
+    expect(res.body).toHaveProperty('id');
+    expect(groups.length).toEqual(countGroups + 1);
+  });
+
+  it('DELETE: /group/:id - should delete group', async () => {
+    const countGroups = groups.length;
+    const id = 'd0ddd9b0-503d-11ec-9374-d5aa7bf3c4d2';
+    const res = await request(app)
+      .delete(`/group/${id}`)
+      .set('Authorization', token);
+    expect(res.status).toBe(200);
+    expect(groups.length).toEqual(countGroups - 1);
   });
 });
